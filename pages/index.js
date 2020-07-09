8cc052c26f7c3466718ed2ba96a24de54e264ab3
phuchealer91 <pepegvcn2@gmail.com>
2020-07-09 09:46:36 +0700

    done

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.scss'
+import { getSortedPostsData } from '../lib/posts';
+import Date from "../components/date";
+import Link from 'next/link'
+
+export default function Home({ allPostsData }) {
+    console.log(allPostsData);
+    return (
+        <Layout home>
+            <Head>
+                          <title>{siteTitle}</title>
+                        </Head>
+                    <section className={utilStyles.headingMd}>
+                      <p>Hello, I’m Phuc</p>
+                        <p>I’m a software engineer. You can contact me on <a href="https://www.facebook.com/phuctailatui147">Facebook</a></p>
+                    </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href="/posts/[id]" as={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    )
+}
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
